fff5f5bb9d002f843fdbc5f197bdc98df9438c5e
pdemidyuk <pdemidyuk@infoblox.com>
2019-12-11 18:40:47 +0300

    ping

--- test.js
+++ test.js
@@ -1,25 +1,24 @@
-
 class  Test {
 
   constructor () {}
 
   getInfo () { // Extended information
     return {
-      id :  ' test ' ,
-      name :  ' Test ' , // extension name
+      id :  'test' ,
+      name :  'Test' , // extension name
       blocks : [ // Definition of each block
         {
-          opcode :  ' hello ' , // When this block is executed, a function called hello is called
+          opcode :  'hello' , // When this block is executed, a function called hello is called
           blockType :  Scratch . BlockType . COMMAND ,　// Normal command block like "Move 10 steps"
-          text :  ' hello '  // Text displayed in the block
+          text :  'hello'  // Text displayed in the block
         }
       ]
     }
   }
 
   hello () {
-    console . log ( ' hello ' ); // Output hello to console log
+    console . log ( 'hello' ); // Output hello to console log
   }
 }
 
-Scratch . Extensions . Register ( New  Test ());
+Scratch.Extensions.Register( New  Test());
