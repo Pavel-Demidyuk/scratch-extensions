b53124a61a42f1b38b66f2927bd1d5ea17a93006
Pavel Demidyuk <3128914@gmail.com>
2019-12-11 18:38:32 +0300

    Update test.js

---
+++ test.js
@@ -0,0 +1,25 @@
+
+class  Test {
+
+  constructor () {}
+
+  getInfo () { // Extended information
+    return {
+      id :  ' test ' ,
+      name :  ' Test ' , // extension name
+      blocks : [ // Definition of each block
+        {
+          opcode :  ' hello ' , // When this block is executed, a function called hello is called
+          blockType :  Scratch . BlockType . COMMAND ,　// Normal command block like "Move 10 steps"
+          text :  ' hello '  // Text displayed in the block
+        }
+      ]
+    }
+  }
+
+  hello () {
+    console . log ( ' hello ' ); // Output hello to console log
+  }
+}
+
+Scratch . Extensions . Register ( New  Test ());
